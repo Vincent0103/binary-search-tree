5bdd58c7305ab28eb5c9187fac6e2a66bc751544
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-09-10 14:00:11 +0200

    Add insert BST function

--- src/BST.js
+++ src/BST.js
@@ -2,22 +2,38 @@
 import mergeSort from './mergeSort.js';
 import removeDuplicates from './removeDuplicates.js';
 
-function Node(data, left = null, right = null) {
-  return { data, left, right };
-}
+const Node = (data, left = null, right = null) => ({ data, left, right });
 
-function Tree(arr) {
-  this.root = buildTree(arr);
-  return { root };
-}
-
-function buildTree(arr) {
+const buildTree = (arr) => {
   const len = arr.length;
   if (len - 1 < 0) return null;
   const mid = Math.floor((len - 1) / 2);
   const root = Node(arr[mid], buildTree(arr.slice(0, mid)), buildTree(arr.slice(mid + 1, len)));
   return root;
-}
+};
+
+const Tree = (arr) => {
+  const root = buildTree(arr);
+
+  function insert(val, currentRoot = root) {
+    if (val > currentRoot.data) {
+      if (!currentRoot.right) {
+        const rightRoot = currentRoot;
+        rightRoot.right = Node(val);
+        return root;
+      }
+      return insert(val, currentRoot.right);
+    }
+    if (!currentRoot.left) {
+      const leftRoot = currentRoot;
+      leftRoot.left = Node(val);
+      return root;
+    }
+    return insert(val, currentRoot.left);
+  }
+
+  return { root, insert };
+};
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
   if (node === null) {
@@ -34,5 +50,7 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 
 const list = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const alterList = removeDuplicates(mergeSort(list));
-const treeNode = buildTree(alterList);
-console.log(prettyPrint(treeNode));
+const treeNode = Tree(alterList);
+// console.log(prettyPrint(treeNode.root));
+console.log(prettyPrint(treeNode.insert(18)));
+console.log(prettyPrint(treeNode.insert(119)));
